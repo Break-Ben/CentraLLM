5d50f08a5a961a84630ab3276fac5cd7300e9007
Break <80194912+Break-Ben@users.noreply.github.com>
2026-07-29 16:33:33 +0800

    Update branding

--- src/renderer/src/pages/home/home-page.tsx
+++ src/renderer/src/pages/home/home-page.tsx
@@ -1,14 +1,11 @@
-import CentraLLMLogo from '@/assets/logo.svg?react'
+import CentraLLMLogo from '@/assets/full-logo.png'
 import { ProviderList } from '@/pages/home/provider-list'
 
 export function HomePage(): React.JSX.Element {
   return (
     <div className="flex items-center p-4 h-full w-full overflow-y-auto scrollbar-none">
       <div className="flex flex-col items-center gap-8 my-auto w-full">
-        <div className="flex flex-col items-center gap-4">
-          <CentraLLMLogo className="h-16 w-16" />
-          <h1 className="text-4xl font-bold tracking-tight">CentraLLM</h1>
-        </div>
+        <img src={CentraLLMLogo} alt="Word Art" className="h-16 w-auto" />
         <ProviderList />
       </div>
     </div>
